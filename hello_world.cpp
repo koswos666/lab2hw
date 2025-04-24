52a533a7e3ff6e593eb1389330feaca066d53af3
koswos666 <kostikovassveta@gmail.com>
2025-04-24 15:22:26 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>  
 int main() {
-   std:: string name;
-   std:: cout << "Enter your name: ";
+   std:: string name;//name
+   std:: cout << "Enter your name: ";// user name
    std:: cin >> name;
-   std:: cout << "Hello world from " << name <<std:: endl;
+   std:: cout << "Hello world from " << name <<std:: endl;//result
     return 0; 
 }
